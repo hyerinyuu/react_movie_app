9f7fb0b3e360e7c74132288806ed7fcd7015842c
hyerin.yuu <hyerin.you@gmail.com>
2020-06-15 15:42:48 +0900

    add README

--- build/precache-manifest.52f0868fef6486a08d490126ed12c33b.js
+++ build/precache-manifest.52f0868fef6486a08d490126ed12c33b.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "d1f125a24a2401d21cedad875dcd529f",
+    "revision": "1a3f59909be60397f4db7a80e3355f48",
     "url": "/react_movie_app/index.html"
   },
   {
-    "revision": "d39e0c28075502da9333",
-    "url": "/react_movie_app/static/css/main.da8f464c.chunk.css"
+    "revision": "1600e267e256493b1da5",
+    "url": "/react_movie_app/static/css/main.8569de73.chunk.css"
   },
   {
     "revision": "fb5478c82c8ce1093474",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/react_movie_app/static/js/2.1aa5c32c.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "d39e0c28075502da9333",
-    "url": "/react_movie_app/static/js/main.4fa07b41.chunk.js"
+    "revision": "1600e267e256493b1da5",
+    "url": "/react_movie_app/static/js/main.415fdab6.chunk.js"
   },
   {
     "revision": "27f83d4c51b444bf3ea9",
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -3,12 +3,24 @@ import { Link } from "react-router-dom";
 import "./Navigation.css";
 
 function Navigation(){
-    return(
-        <div className="nav">
-            <Link to="/">Home</Link>
-            <Link to="/about">About</Link>
+    /*
+                React에서 아래와 같은 href tag를 사용하지 않는 이유
+             html은 기본적으로 page를 모두 새로 reload(== 페이지를 강제로 새로고침하고 리액트 component를 죽임) 시키기 때문에 react의 철학과 전혀 맞지 않음.
+             게다가 rootPath도 없어서 /about으로 가면 그냥 localhost:8080/about으로 가기 때문에 그런 페이지는 안나옴(우리가 원하는 path가 아님)
+        <div>
+            <a href="/">Home</a>
+            <a href="/about">About</a>
         </div>
-    );
+        
+        따라서 Link를 import해서 사용하면 됨
+        (Link는 Router밖에서는 작동 안됨 항상 Router 안에서만 사용 가능)
+        */
+    return(
+    <div className="nav">
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+    </div>
+    )
 }
 
 export default Navigation;
